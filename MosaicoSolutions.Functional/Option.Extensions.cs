ee3e5c8629c222cff2fc244c0ad514c38d6b1aa7
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-09-14 01:25:34 -0300

    initial commit.

--- MosaicoSolutions.Functional/Option.Extensions.cs
+++ MosaicoSolutions.Functional/Option.Extensions.cs
@@ -6,35 +6,44 @@ namespace MosaicoSolutions.Functional.Extensions
     {
         public static Option<T> Of<T>(T value) =>
             value;
+
         public static Option<T> Some<T>(T value) =>
             value == null
                 ? throw new InvalidOperationException("Some method can not be called with a null value.")
                 : value;
+
         public static Option<NoneType> NoneValue => 
             None<NoneType>();
+
         public static Option<T> None<T>() =>
             new Option<T>();
+
         public struct NoneType { }
+
         public static TResult Match<TResult, T>(this Option<T> @this,
                                                 Func<T, TResult> some,
                                                 Func<TResult> none) =>
             @this.HasValue
                 ? some(@this.Value)
                 : none();
+
         public static T OrElse<T>(this Option<T> @this,
                                   T other) =>
             @this.OrElse(() => other);
+
         public static T OrElse<T>(this Option<T> @this,
                                   Func<T> other) =>
             @this.Match(
                 some: _ => _,
                 none: other
             );
+
         public static T OrElseThrow<T, TException>(this Option<T> @this,
                                                    TException exception) 
                                                    where TException : Exception
                                                    =>
             @this.OrElseThrow(() => exception);
+
         public static T OrElseThrow<T, TException>(this Option<T> @this,
                                                    Func<TException> exception) 
                                                    where TException : Exception
@@ -42,6 +51,7 @@ namespace MosaicoSolutions.Functional.Extensions
             @this.HasValue
                 ? @this.Value
                 : throw exception();
+
         public static Option<TResult> Map<TResult, T>(this Option<T> @this,
                                                       Func<T, TResult> map)
                                                       =>
@@ -55,7 +65,7 @@ namespace MosaicoSolutions.Functional.Extensions
             @this.HasValue
                 ? bind(@this.Value)
                 : Option.None<TResult>();
-
+                
         public static Option<T> Where<T>(this Option<T> @this,
                                                         Predicate<T> predicate)
                                                         =>
--- MosaicoSolutions.Functional/Option.cs
+++ MosaicoSolutions.Functional/Option.cs
@@ -5,30 +5,37 @@ namespace MosaicoSolutions.Functional
     public struct Option<T>
     {
         private readonly T _value;
+
         public bool HasValue { get; }
+
         public T Value =>
             HasValue
                 ? _value
                 : throw new InvalidOperationException("No such element!");
+
         public T ValueOrDefault =>
             HasValue
                 ? _value
                 : default(T);
+
         internal Option(T value, bool hasValue)
         {
             _value = value;
             HasValue = hasValue;
         }
+
         public void IfPresent(Action<T> some)
         {
             if(HasValue)
                 some(_value);
         }
+
         public void IfNone(Action none)
         {
             if(!HasValue)
                 none();
         }
+        
         public void Match(Action<T> some, Action none)
         {
             if(HasValue)
